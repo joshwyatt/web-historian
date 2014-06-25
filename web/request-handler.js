7fc6b8661675423dd55df75952ea8ed1a6a35ae8
hackystack <dorpalenbarry@gmail.com>
2014-06-25 15:03:19 -0700

    Started building request helper functions

--- web/request-handler.js
+++ web/request-handler.js
@@ -3,5 +3,21 @@ var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var statusCode = 404;
+
+  var handler = {
+  	POST: function(req, res){
+
+  	},
+  	GET: function(req, res){
+		statusCode = 200;
+  	},
+  	OPTIONS: function(){}
+  };
+
+  var method = req.method;
+  console.log(req.method);
+  handler[method](req, res);
+
+  res.end(statusCode, archive.paths.list);
 };
